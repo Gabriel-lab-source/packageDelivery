90f0f16eb40d1a8146cf73efdde140d7d2642ed6
maquinaLixo <gabriel_amirati@hotmail.com>
2026-04-07 16:15:32 -0300

    Adicionando rotas e CRUD

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,15 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///delivery.db"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # Desativa sistema que monitora objetos do banco
+
+    db.init_app(app)
+
     from app.routes import init_routes
     init_routes(app)
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///delivery.db"
-    db.init_app(app)
+
     with app.app_context():
         db.create_all()
     return app
--- app/routes.py
+++ app/routes.py
@@ -17,68 +17,151 @@ def init_routes(app):
 
     @app.route("/drivers", methods=["POST"])
     def insert_drivers():
+
         data = request.get_json()
+
         driver = Driver(
             name=data.get("name"),
             phone=data.get("phone"),
             vehicle=data.get("vehicle"))
+
         db.session.add(driver)
+
         db.session.commit()
+
         return {"message": "created"}, 201
 
     @app.route("/drivers", methods=["GET"])
     def list_drivers():
+
         drivers = db.session.scalars(select(Driver)).all()
+
         dictDrivers = [driver.to_dict() for driver in drivers]
+
         return jsonify(dictDrivers)
 
     @app.route("/drivers/<int:id>", methods=["GET"])
     def get_driver(id):
+
         driver = db.session.get(Driver, id)
+
         if not driver:
             return {"error": "Driver not found"}, 404
+
         result = driver.to_dict()
+
         return jsonify(result)
 
     @app.route("/drivers/<int:id>", methods=["PUT"])
     def edit_driver(id):
+
         driver = db.session.get(Driver, id)
+
         if not driver:
             return {"error": "Driver not found"}, 404
+
         data = request.get_json()
+
         driver.name = data.get("name", driver.name)
         driver.phone = data.get("phone", driver.phone)
         driver.vehicle = data.get("vehicle", driver.vehicle)
         driver.active = data.get("active", driver.active)
+
         db.session.commit()
+
         result = driver.to_dict()
+
         return jsonify(result), 200
 
     @app.route("/drivers/<int:id>", methods=["DELETE"])
     def delete_driver(id):
+
         driver = db.session.get(Driver, id)
+
         if not driver:
             return {"error": "Driver not found"}, 404
+
         db.session.delete(driver)
+
         db.session.commit()
+
         return {"message": "deleted"}
 
     @app.route("/packages", methods=["POST"])
     def insert_packages():
         data = request.get_json()
+
         driver = db.session.get(Driver, data.get("driver_id"))
+
         package = Package(
             description=data.get("description"),
             address=data.get("address"),
             status=data.get("status"),
             driver=driver
         )
+
         db.session.add(package)
+
         db.session.commit()
+
         return {"message": "created"}, 201
 
     @app.route("/packages", methods=["GET"])
     def list_packages():
+
         packages = db.session.scalars(select(Package)).all()
         dict_packages = [package.to_dict() for package in packages]
         return jsonify(dict_packages)
+
+    @app.route("/packages/<int:id>", methods=["GET"])
+    def get_package(id):
+
+        package = db.session.get(Package, id)
+
+        if not package:
+            return {"error": "Package not found"}, 404
+
+        result = package.to_dict()
+        return jsonify(result)
+
+    @app.route("/packages/<int:id>", methods=["PUT"])
+    def edit_package(id):
+
+        package = db.session.get(Package, id)
+
+        if not package:
+            return {"error": "Package not found"}, 404
+
+        data = request.get_json()
+
+        if "description" in data:
+            package.description = data["description"]
+
+        if "address" in data:
+            package.address = data["address"]
+
+        if "status" in data:
+            package.status = data["status"]
+
+        if "driver_id" in data:
+            package.driver_id = data["driver_id"]
+
+        db.session.commit()
+
+        return jsonify(package.to_dict())
+
+    @app.route("/drivers/<int:id>", methods=["DELETE"])
+    def delete_package(id):
+
+        package = db.session.get(Package, id)
+
+        if not package:
+            return {"error": "Package not found"}, 404
+
+        db.session.delete(package)
+
+        db.session.commit()
+
+        return {"message": "deleted"}
+
+    
